b2c2793d5d4f7c6fda5ca73f5eedfe8fae400815
Euan Murray <euan.murray@itftennis.com>
2018-10-18 17:14:06 +0100

    Burger-App

---
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Auxilliary from "../../hoc/Auxiliary";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Auxilliary>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </Auxilliary>
+    );
+  }
+}
+
+export default BurgerBuilder;
