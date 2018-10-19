2124793a9404a66b314e526d1638e9d5078b2bd0
Euan Murray <euan.murray@itftennis.com>
2018-10-19 17:08:04 +0100

    no message

--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import Auxilliary from "../../hoc/Auxiliary";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
   render() {
     return (
       <Auxilliary>
-        <div>Burger</div>
-        <div>Build Controls</div>
+        <Burger />
       </Auxilliary>
     );
   }
